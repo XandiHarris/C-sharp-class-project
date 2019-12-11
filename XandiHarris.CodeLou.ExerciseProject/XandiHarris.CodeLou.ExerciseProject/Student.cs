3a4adc5e7de2627c8d0ac0a283ef3d87a56ac7d3
Xandi Harris <xandi.harris@gmail.com>
2019-12-11 13:29:37 -0500

    Updated Project 1, added Project 2 and Project 3

--- XandiHarris.CodeLou.ExerciseProject/XandiHarris.CodeLou.ExerciseProject/Student.cs
+++ XandiHarris.CodeLou.ExerciseProject/XandiHarris.CodeLou.ExerciseProject/Student.cs
@@ -6,58 +6,14 @@ namespace XandiHarris.CodeLou.ExerciseProject
 {
     class Student
     {
-        public int StudentId {get; set;}
-        public string FirstName {get; set;}
-        public string LastName {get; set;}
-        public string ClassName {get; set;}
-        public DateTimeOffset StartDate {get; set;}
-        public string LastClassCompleted {get; set;}
-        public DateTimeOffset LastClassCompletedOn {get; set;}
-        static void Main(string[] _1)
-        {
-            Console.WriteLine("Would you like to enter a New Student? y/n");
-            var newStudent = Console.ReadLine();
-            var students = new List<Student>();
-
-            while (newStudent == "y")
-            {
-                Console.WriteLine("Enter Student Id");
-                var studentId = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Enter First Name");
-                var studentFirstName = Console.ReadLine();
-                Console.WriteLine("Enter Last Name");
-                var studentLastName = Console.ReadLine();
-                Console.WriteLine("Enter Class Name");
-                var className = Console.ReadLine();
-                Console.WriteLine("Enter Last Class Completed");
-                var lastClass = Console.ReadLine();
-                Console.WriteLine("Enter Last Class Completed Date in format MM/dd/YYYY");
-                var lastCompletedOn = DateTimeOffset.Parse(Console.ReadLine());
-                Console.WriteLine("Enter Start Date in format MM/dd/YYYY");
-                var startDate = DateTimeOffset.Parse(Console.ReadLine());
-
-                var studentRecord = new Student();
-                studentRecord.StudentId = studentId;
-                studentRecord.FirstName = studentFirstName;
-                studentRecord.LastName = studentLastName;
-                studentRecord.ClassName = className;
-                studentRecord.StartDate = startDate;
-                studentRecord.LastClassCompleted = lastClass;
-                studentRecord.LastClassCompletedOn = lastCompletedOn;
-
-                students.Add(studentRecord);
-
-
-                Console.WriteLine("Would you like to enter another new student? y/n");
-                newStudent = Console.ReadLine();
-                Console.WriteLine();
-            }
-            Console.WriteLine($"Student Id | Name |  Class  Started On | Last Class Finished On |"); ;
-
-            foreach (var studentRecord in students)
-            {
-                Console.WriteLine($"{studentRecord.StudentId} | {studentRecord.FirstName} {studentRecord.LastName} | {studentRecord.ClassName}  {studentRecord.StartDate}  |  {studentRecord.LastClassCompleted}  {studentRecord.LastClassCompletedOn} "); ;
-            }    
-        }
+        public  int StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ClassName { get; set; }
+        public DateTimeOffset StartDate { get; set; }
+        public string LastClassCompleted { get; set; }
+        public DateTimeOffset LastClassCompletedOn { get; set; }
+         
     }
+    
 }
---
+++ XandiHarris.CodeLou.ExerciseProject/XandiHarris.CodeLou.ExerciseProject/program.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using System.Linq;
+using System.IO;
+
+namespace XandiHarris.CodeLou.ExerciseProject
+{
+    class Program
+    {
+        public static List<Student> students = new List<Student>();
+        public static Dictionary<string, Student> studentDict = new Dictionary<string, Student>();
+
+        static void Main()
+        {
+            bool showMenu = true;
+            LoadStudents();
+            while (showMenu)
+            {
+                showMenu = MainMenu();
+            }
+        }
+        public static bool MainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Menu");
+            Console.WriteLine("1. New Student");
+            Console.WriteLine("2. List Students");
+            Console.WriteLine("3. Find Students By Name");
+            Console.WriteLine("4. Find Students By Class");
+            Console.WriteLine("5. Exit");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    NewStudentEntry();
+                    return true;
+                case "2":
+                    ListStudents();
+                    return true;
+                case "3":
+                    SearchStudents();
+                    return true;
+                case "4":
+                    SearchClasses();
+                    return true;
+                case "5":
+                    return false;
+                default:
+                    return true;
+            }
+        }
+        public static void NewStudentEntry()
+        {
+            Console.WriteLine("Would you like to enter a New Student? y/n");
+            string enterStudent = Console.ReadLine();
+
+            while (enterStudent == "y")
+            {
+                bool validated = false;
+                int studentId;
+                do
+                {
+                    Console.WriteLine("Enter Student ID Number");
+                    validated = int.TryParse(Console.ReadLine(), out studentId);
+                    if (validated)
+                    {
+                        if (students.Exists(s => s.StudentId == studentId))
+                        {
+                            Console.WriteLine("Student already exists in registry with this ID number.");
+                            validated = false;
+                        }
+                    }
+                } while (!validated);
+                Console.WriteLine("Enter First Name");
+                var studentFirstName = Console.ReadLine();
+                Console.WriteLine("Enter Last Name");
+                var studentLastName = Console.ReadLine();
+                Console.WriteLine("Enter Class Name");
+                var className = Console.ReadLine();
+                validated = false;
+                DateTimeOffset startDate;
+                do
+                {
+                    Console.WriteLine("Enter Class Start Date");
+                    validated = DateTimeOffset.TryParse(Console.ReadLine(), out startDate);
+                } while (!validated);
+                Console.WriteLine("Enter Last Course Completed");
+                var lastClass = Console.ReadLine();
+                validated = false;
+                DateTimeOffset lastCompletedOn;
+                do
+                {
+                    Console.WriteLine("Enter Last Course Completion Date");
+                    validated = DateTimeOffset.TryParse(Console.ReadLine(), out lastCompletedOn);
+                    if (validated)
+                    {
+                        if (lastCompletedOn >= startDate)
+                        {
+                            Console.WriteLine("Course must have been completed before the current course start date!");
+                            validated = false;
+                        }
+                    }
+                } while (!validated);
+
+                var newStudent = new Student
+                {
+                    StudentId = studentId,
+                    FirstName = studentFirstName,
+                    LastName = studentLastName,
+                    ClassName = className,
+                    StartDate = startDate,
+                    LastClassCompleted = lastClass,
+                    LastClassCompletedOn = lastCompletedOn
+                };
+
+                students.Add(newStudent);
+                studentDict.Add(newStudent.FirstName + " " + newStudent.LastName, newStudent);
+
+                Console.WriteLine("Would you like to enter another new student? y/n");
+                enterStudent = Console.ReadLine();
+                Console.WriteLine();
+            }
+            SaveStudents();
+        }
+        public static void ListStudents()
+        {
+            foreach (var student in students)
+            {
+                Console.WriteLine("");
+                Console.WriteLine($"Student ID: {student.StudentId}");
+                Console.WriteLine($"Student name: {student.FirstName} {student.LastName}");
+                Console.WriteLine($"Current class: {student.ClassName} started {student.StartDate}");
+                Console.WriteLine($"Previous class: {student.LastClassCompleted} finished {student.LastClassCompletedOn}");
+                Console.WriteLine("");
+            }
+            Console.WriteLine("Press any Key to return to menu");
+            Console.ReadKey();
+        }
+        public static void ListStudents(Student student)
+        {
+            Console.WriteLine("");
+            Console.WriteLine($"Student ID: {student.StudentId}");
+            Console.WriteLine($"Student name: {student.FirstName} {student.LastName}");
+            Console.WriteLine($"Current class: {student.ClassName} started {student.StartDate}");
+            Console.WriteLine($"Previous class: {student.LastClassCompleted} finished {student.LastClassCompletedOn}");
+            Console.WriteLine("");
+            Console.WriteLine("Press any Key to return to menu");
+            Console.ReadKey();
+        }
+        public static void ListStudents(IEnumerable<Student> selectedStudents)
+        {
+            foreach (Student student in selectedStudents)
+            {
+                ListStudents(student);
+            }
+        }
+
+        public static void SearchStudents()
+        {
+            Console.WriteLine("Please enter a Student's FIRST or LAST NAME to search for.");
+            var searchName = Console.ReadLine();
+            var selectedStudents = students.Where(s => s.FirstName.IndexOf(searchName, StringComparison.CurrentCultureIgnoreCase) >= 0 || s.LastName.IndexOf(searchName, StringComparison.CurrentCultureIgnoreCase) >= 0);
+            if (selectedStudents.Any())
+            {
+                ListStudents(selectedStudents);
+            }
+            else
+            {
+                Console.WriteLine($"{searchName} Is not found in student records. Please check spelling of Name.");
+            }
+        }
+        public static void SearchClasses()
+        {
+            Console.WriteLine("Please enter a CLASS to search for.");
+            var searchName = Console.ReadLine();
+            var selectedStudents = students.Where(s => s.ClassName.IndexOf(searchName, StringComparison.CurrentCultureIgnoreCase) >= 0);
+            if (selectedStudents.Any())
+            {
+                ListStudents(selectedStudents);
+            }
+            else
+            {
+                Console.WriteLine($"{searchName} not found in class records. Please check spelling of Class.");
+            }
+        }
+        public static void SaveStudents()
+        {
+            var serializer = new JsonSerializer();
+            string fileName = "Students.JSON";
+            using (var writer = new StreamWriter(fileName))
+            using (var jsonWriter = new JsonTextWriter(writer))
+            {
+                serializer.Serialize(jsonWriter, students);
+            }
+        }
+        public static void LoadStudents()
+        {
+            string fileName = "Students.JSON";
+            if (File.Exists(fileName))
+            {
+                var serializer = new JsonSerializer();
+                using (var reader = new StreamReader(fileName))
+                using (var jsonReader = new JsonTextReader(reader))
+                {
+                    students = serializer.Deserialize<List<Student>>(jsonReader);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Students database not found on disk.  Will be created upon adding students.");
+            }
+        }
+    }
+}
